f9bc80cfdabedf7df34a511378dc2eb57331aa69
ashismb105 <43338439+ashismb105@users.noreply.github.com>
2026-06-01 09:51:36 +0530

    Create OrderRepository interface for Order entity

--- OrderRepository.java
+++ OrderRepository.java
@@ -1,1 +1,7 @@
+package com.tradingengine.repository;
 
+import com.tradingengine.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
